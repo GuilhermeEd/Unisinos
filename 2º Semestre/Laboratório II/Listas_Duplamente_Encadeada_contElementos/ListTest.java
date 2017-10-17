02e49145b0613def359d653d019a77fef1adc887
WillhelmEdward <guilherme.eduardo92@hotmail.com>
2017-10-17 19:03:53 -0200

    Added Tarefa de Laboratório II

---
+++ 2º Semestre/Laboratório II/Listas_Duplamente_Encadeada_contElementos/ListTest.java
@@ -0,0 +1,60 @@
+
+
+public class ListTest {
+
+   public static void main( String args[] ) {
+      List list = new List();  // create the List container
+
+      // objects to store in list
+      Boolean bool = Boolean.TRUE;
+      Character character = new Character( '$' );
+      Integer integer = new Integer( 34567 );
+      String string = "hello";
+
+      // insert references to objects in list
+      list.insertAtFront( bool );
+      list.print();
+      list.insertAtFront( character );
+      list.print();
+      list.insertAtBack( integer );
+      list.print();
+      list.insertAtBack( string );
+      list.print();
+      list.insertAtBack("1");
+      list.insertAtBack("2");
+      list.insertAtBack("3");
+      list.insertAtBack("4");
+      list.print();
+
+
+      // remove objects from list; print after each removal
+      try { 
+        Object removedObject = list.removeFromFront();
+         System.out.println( removedObject.toString() + " removed" );
+         list.print();
+
+         removedObject = list.removeFromFront();
+         System.out.println( removedObject.toString() + " removed" );
+         list.print();
+
+         removedObject = list.removeFromBack();
+         System.out.println( removedObject.toString() + " removed" );
+         list.print();
+
+         removedObject = list.removeFromBack();
+         System.out.println( removedObject.toString() + " removed" );
+         list.print(); 
+         
+         //list.remove (integer);
+         //list.print();
+
+      } // end try block
+
+      // catch exception if remove is attempted on an empty List 
+      catch ( EmptyListException emptyListException ) {
+         emptyListException.printStackTrace();
+      }
+   }
+
+} // end class ListTest
+
